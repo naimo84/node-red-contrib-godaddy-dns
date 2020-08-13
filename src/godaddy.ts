068a4807c3ca71072accd8da4ac06690b09a07bf
naimo84 <git@neumann-benjamin.de>
2020-08-13 06:39:28 +0200

    feat: rmoved dependecy

--- src/godaddy.ts
+++ src/godaddy.ts
@@ -1,6 +1,54 @@
 
 import { Red, Node } from 'node-red';
-import dns = require("godaddy-dns");
+import request = require('request-promise');
+
+async function getCurrentIp() {
+    return request('https://api.ipify.org/')
+}
+
+async function updateRecords(ip, config) {
+    let recordDefaults = {
+        type: 'A',
+        data: ip,
+        ttl: 60 * 10 // 10 minutes (minimum allowed)
+    }
+
+    let records = config.records
+    // if records is a single object or string wrap it into an array
+    if (records.constructor !== Array) {
+        records = [records]
+    }
+    records = records.map((record) => {
+        // if current record is a single string
+        // wrap it in array
+        if (typeof record === 'string') {
+            record = { name: record }
+        }
+        return Object.assign({}, recordDefaults, record)
+    })
+
+    return Promise.all(
+        records.map((record) => {
+            let domain = config.domain
+            if (typeof (record.domain) !== 'undefined') {
+                domain = record.domain
+                delete record.domain
+            }
+            let options = {
+                method: 'PUT',
+                url: `https://api.godaddy.com/v1/domains/${domain}/records/${record.type}/${record.name.replace('@', '%40')}`,
+                headers: {
+                    'authorization': `sso-key ${config.apiKey}:${config.secret}`,
+                    'content-type': 'application/json'
+                },
+                body: [record],
+                json: true
+            }
+            return request(options)
+        })
+    )
+}
+
 
 module.exports = function (RED: Red) {
     function godaddyNode(config: any) {
@@ -30,36 +78,40 @@ module.exports = function (RED: Red) {
 
     async function getDns(node, outMsg) {
         try {
-            let currentIp = await dns.getCurrentIp();
-            let items = node.items;
-            if (outMsg.payload) {
-                if (outMsg.payload.newIp) {
-                    currentIp = outMsg.payload.newIp;
+            let currentIp = await getCurrentIp();
+            let lastIp = node.context().get('lastIp');
+            if (lastIp && lastIp != currentIp) {
+                let items = node.items;
+                if (outMsg.payload) {
+                    if (outMsg.payload.newIp) {
+                        currentIp = outMsg.payload.newIp;
+                    }
+                    if (outMsg.payload.entries) {
+                        items = outMsg.payload.entries;
+                    }
                 }
-                if (outMsg.payload.entries) {
-                    items = outMsg.payload.entries;
+                for (let item of items) {
+                    await updateRecords(currentIp, {
+                        "apiKey": node.config.key,
+                        "secret": node.config.secret,
+                        "domain": item.domain,
+                        "records": [
+                            { "type": item.type, "name": item.name, "ttl": parseInt(item.ttl) }
+                        ]
+                    });
+                    node.debug(`[${new Date()}] Successfully updated DNS records to ip ${currentIp}`);
                 }
+                node.context().set('lastIp', currentIp);
+                outMsg.dnsUpdate = true;
+            } else {
+                outMsg.dnsUpdate = false;
             }
-            for (let item of items) {
-                await dns.updateRecords(currentIp, {
-                    "apiKey": node.config.key,
-                    "secret": node.config.secret,
-                    "domain": item.domain,
-                    "records": [
-                        { "type": item.type, "name": item.name, "ttl": parseInt(item.ttl) }
-                    ]
-                });
-                node.debug(`[${new Date()}] Successfully updated DNS records to ip ${currentIp}`);
-            }
-
             outMsg.currentIp = currentIp;
-            outMsg.dnsUpdate = true;
             node.send(outMsg);
         }
         catch (err) {
             if (err && err.message !== 'Nothing to update') {
                 node.error(`[${new Date()}] ${err}`)
-
             }
             outMsg.dnsUpdate = false;
             node.send(outMsg);
