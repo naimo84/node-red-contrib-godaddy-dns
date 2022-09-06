23db0acfbb0494fa4babca96ff51869c63df25c3
naimo84 <git@naimo84.dev>
2022-09-06 06:48:50 +0200

    build: changed gulp logic

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,58 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+
 var sourcemaps = require('gulp-sourcemaps');
 var nodemon = require('gulp-nodemon');
 var watch = require('gulp-watch');
 
 var paths = {
     pages: ['src/*.html'],
-    assets: ['src/icons/*.png'],
     src: 'src',
     dist: 'dist'
 };
 
 function copyHtml() {
-    return gulp.src(paths.pages, { base: paths.src })    
+    return gulp.src(paths.pages, { base: paths.src })
         .pipe(gulp.dest(paths.dist));
 }
 
 gulp.task("copy-html", copyHtml);
 
-gulp.task("copy-assets", function () {
-    return gulp.src(paths.assets)
-        .pipe(gulp.dest(paths.dist + "/icons"));
-});
-
 gulp.task('develop', function (done) {
     var stream = nodemon({
-        legacyWatch:true,
+        legacyWatch: true,
+        exec: 'node --inspect=9229 --preserve-symlinks      --experimental-modules       --trace-warnings     /usr/lib/node_modules/node-red/red.js',
         ext: '*.js',
         watch: [paths.dist],
         ignore: ["*.map"],
-        done: done,        
-        verbose: true
+        done: done,
+        verbose: true,
+        delay: 2000,
+        env: { "NO_UPDATE_NOTIFIER": "1" }
     });
 
     copyHtml();
+    var tsProject = ts.createProject("tsconfig.json");
+    tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.dist));
 
     watch(paths.pages).on('change', () => {
+        copyHtml();
+        stream.emit('restart', 10)
+    });
+
+    watch('src/*.ts').on('change', () => {
+        tsProject.src()
+            .pipe(sourcemaps.init())
+            .pipe(tsProject())
+            .js
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(paths.dist));
+
         stream.emit('restart', 10)
     });
 
@@ -52,8 +68,8 @@ gulp.task('develop', function (done) {
 
 gulp.task("default", gulp.series(
     gulp.parallel('copy-html'),
-    gulp.parallel('copy-assets'),
     () => {
+        var tsProject = ts.createProject("tsconfig.json");
         return tsProject.src()
             .pipe(sourcemaps.init())
             .pipe(tsProject())
